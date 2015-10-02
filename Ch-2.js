52cc62282013957645fa38d03dd1cad1ace7e4bd
Rohan Paul <rohanpaul2@gmail.com>
2015-10-03 04:47:57 +0530

    small edit

--- Ch-2.js
+++ Ch-2.js
@@ -25,7 +25,7 @@ function loopingTriangle() {
 
 console.log(loopingTriangle());
 
-console.log("End of this solution ***************")
+console.log("***End of this solution***")
 
 /*
 Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz"
@@ -33,15 +33,42 @@ for numbers that are divisible by both 3 and 5.
 */
 
 function fizzBuzz() {
-    for (var i =0; i <=100; i++) {
-        if ((i % 3 == 0) && (i % 5 ==0)) {
+    for (var i = 0; i <= 100; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
             console.log(i + ". FizzBuzz");
-        } else if ( i %3 == 0 ) {
+        } else if ( i %3 === 0 ) {
             console.log(i + ". Fizz");
-        } else if (i % 5 == 0) {
+        } else if (i % 5 === 0) {
             console.log(i + ". Buzz");
         }
     }
 }
 
 console.log(fizzBuzz());
+console.log("***End of this solution***")
+
+/* Chess Board - Write a program that creates a string that represents a grid of given size,
+ using newline characters to separate lines. At each position of the grid there
+ is either a space or a “#” character. The characters should form a chess board.
+ */
+
+console.log("Chess Board");
+
+var matrixSize = 8;
+
+
+for (var i = 0; i < matrixSize; i++) {
+    var matrixValue = "";
+    for (var j = 0; j < matrixSize; j++) {
+        if ((i+j) % 2 === 0) {
+            matrixValue += " ";
+        } else {
+            matrixValue += "#";
+        }
+    }
+    console.log(matrixValue);
+}
+
+
+
+console.log("***End of this solution***")
