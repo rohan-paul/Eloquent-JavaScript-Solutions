e99cb0ce87a1ac252c0b1006554227e9ca4300fd
Rohan Paul <rohanpaul2@gmail.com>
2015-10-08 20:54:03 +0530

    small change

---
+++ Ch-4.js
@@ -0,0 +1,6 @@
+/*
+Write a range function that takes two arguments, start and end, andreturns an array containing all the numbers from start up to (and includ-ing) end.
+    Next, write a sum function that takes an array of numbers and returns thesum of these numbers. Run the previous program and see whether it doesindeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+*/
