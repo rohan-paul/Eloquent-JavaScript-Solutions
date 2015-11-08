b3b6debec0c64c5af690a4bd1e5ef27e74c1ca85
Rohan Paul <rohanpaul2@gmail.com>
2015-11-08 17:58:57 +0530

    adding chap-4/Prob-2

--- Ch-4.js
+++ Ch-4.js
@@ -1,4 +1,4 @@
-/*
+/* Chapter-4/Problem-1
  The introduction of this book alluded to the following as a nice way to com-
  pute the sum of a range of numbers:
  console.log(sum(range(1, 10)));
@@ -33,3 +33,36 @@ function sumOfArray(array) {
 console.log(range(1, 10));
 console.log(range(1, 10, 2));
 console.log(sumOfArray(range(1, 10)));
+
+/* Chapter-4/Problem-2
+ Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+
+ The first, reverseArray, takes an array as an argument and produces a new array that has the same elements in the inverse order.
+
+ The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+*/
+
+function reverseArray(array) {
+    var output = [];
+    for (i = array.length - 1; i >= 0; i--) {
+        output.push(array[i]);
+    }
+
+    return output
+}
+
+function reverseArrayInPlace(array) {
+    var output = [];
+    var loopTimer = array.length; // If I directly put array.length in the below for loop, the program goes out of memory during runtime.
+
+    for (var i = 0; i < loopTimer; i++) {
+        output[i] = array.pop();
+    }
+    for (var j = 0; j < loopTimer; j++) {
+        array.push(output[j]);
+    }
+    return array;
+};
+
+console.log(reverseArray([1 ,2 ,3, 4]));
+console.log(reverseArrayInPlace([5 ,6 ,7, 8]));
