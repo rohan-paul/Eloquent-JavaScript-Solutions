d641b32634f0c81e4fe52353b5b0f796e58ba366
Rohan Paul <rohanpaul2@gmail.com>
2015-11-09 07:54:57 +0530

    ch-4 code for list

--- Ch-4.js
+++ Ch-4.js
@@ -66,3 +66,31 @@ function reverseArrayInPlace(array) {
 
 console.log(reverseArray([1 ,2 ,3, 4]));
 console.log(reverseArrayInPlace([5 ,6 ,7, 8]));
+
+/* Chapter-4/Problem-3
+
+ Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as an argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element. If you haven’t already, also write a recursive version of nth.
+*/
+
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--) {
+      list = {value: array[i], rest: list};
+  }
+   return list;
+};
+
+function listToArray(list) {
+    var array = [];
+    while (list.rest !== null) {
+        array.push(list.value);
+        list = list.rest;
+    }
+    array.push(list.value);
+    return array;
+
+};
+
+
+console.log(arrayToList([10, 20, 30]));
+console.log(listToArray(arrayToList([10, 20, 30])));
